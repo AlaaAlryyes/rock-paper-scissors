625bf4071bfbee4151641b0891a153fcaaf25cfd
AlaaAlryyes <asjr182311@gmail.com>
2023-01-10 21:52:58 +0200

    commit

--- script.js
+++ script.js
@@ -6,6 +6,9 @@ const imgs = {
     "scissors": ""
 }
 
+var computerPoints = 0;
+var playerPoints = 0;
+
 function buildOptionsButtons() {
     let btn = document.createElement("div")
     btn.className = "option-btn"
@@ -15,10 +18,58 @@ function buildOptionsButtons() {
 }
 
 function generateRandomChoice() {
-
+    let c = Math.floor(Math.random() * 4);
+    return options[c];
 }
 
 function determineWinner(playerChoice, computerChoice) {
+    let result = 0;
+    switch (playerChoice) {
+        case "rock":
+            switch (computerChoice) {
+                case "rock":
+                    result = 0;
+                    break
+                case "paper":
+                    result = -1;
+                    break
+                case "scissors":
+                    result = 1;
+                    break
+            }
+            break;
+        case "paper":
+            switch (computerChoice) {
+                case "rock":
+                    result = 1;
+                    break
+                case "paper":
+                    result = 0;
+                    break
+                case "scissors":
+                    result = -1;
+                    break
+            }
+            break
+        case "scissors":
+            switch (computerChoice) {
+                case "rock":
+                    result = -1;
+                    break
+                case "paper":
+                    result = 1;
+                    break
+                case "scissors":
+                    result = 0;
+                    break
+            }
+            break
+    }
 
+}
+
+function reset() {
+    computerPoints = 0;
+    playerPoints = 0;
 }
 
